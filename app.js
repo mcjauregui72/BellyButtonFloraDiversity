4fbcbdcc5e9600db166a78e2ad6165ce12e497eb
M. Christine Jauregui <christine.jauregui@merriampark.org>
2025-06-29 19:57:31 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
 
 //Instead of importing data from url, reference samples.json file saved in repository root
 //which is recommended to make dashboard self-contained when using Pages 
-//A. Define urlData for import file
-//const urlData = "https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json";
+//A. Define urlData for import file: const urlData = "https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json";
 //B. Promise pending for json data from urlData not necessary since samples.json saved in repo root
 //const dataPromise = d3.json(urlData);
   //console.log("Data promise: ", dataPromise);
 
 const urlData = "samples.json";
 
-//How to update content of sample-metadata element on webpage: 
-//Define 'metaData' function to take meta_data as parameter
-//Assign 'meta_data' value of 'metadata' key inside json data (portion denoted by 'data.metadata')
-//Define 'meta_data' as variable holding 'sample-metadata' retrieved from json
-//Select html element with div id="sample-metadata" and save to demSelect variable 
+//To update content of sample-metadata element on webpage: 
+//1. Define 'metaData' function to take 'demographicInfo'/meta_data as parameter
+//2. Assign 'meta_data' value of 'metadata' key inside json data (portion denoted by 'data.metadata')
+//3. Define 'meta_data' as variable holding 'sample-metadata' retrieved from json
+//4. Select html element with div id="sample-metadata" and save to demSelect variable 
 
-//Define 'metaDataFunc' to take 'demographicInfo' as parameter 
-//Select "sample-metadata" element from html and save as 'demSelect'
-//Reset 'demSelect' content to 'demographicInfo' template string representing characteristics of selected trial participant's demographic info
+//Select "sample-metadata" element from html and save as 'demSelect', then reset 'demSelect' content to 
+//'demographicInfo' template string representing details of selected trial participant's demographic info
     function metaDataFunc(demographicInfo) {     
       let demSelect = d3.select("#sample-metadata")
       demSelect.html(
@@ -42,22 +40,21 @@ d3.json(urlData)
       
     //Use dot notation to access 'metadata' and 'samples' parts of 'data' object  
     //Define 'meta_data' as metadata key within 'data' object
+    //Define 'samples' as samples key within 'data' object 
       meta_data = data.metadata;
-    //Define 'samples' as samples key within 'data' object  
       samples = data.samples;
     
     //Select html element with id="selDataset" and save in 'selector' variable
     //contains reference to onchange="optionChanged(this.value)">
       let selector = d3.select("#selDataset");
     
-    //.forEach() method iterates over each object in meta_data array one by one,
-    //so '#sample-metadata' element displays only one object at a time
+    //.forEach() method iterates over each object in meta_data array 1 by 1, so '#sample-metadata' element displays only one object at a time
     //Within loop, current object's 'id' permits dynamic creation of option element in dropdown menu
     meta_data.forEach((obj) => {
            let id = obj.id;
+      
     //selector.append("option") appends option elements to selector element to create selectable options
-    //text(id) sets text content of option element, sets how text displays in dropdown menu
-    //.property("value", id) sets value property of option element to value of id
+    //text(id) sets text content of option element (how text displays in dropdown) and .property("value", id) sets value property of option element to value of id
     selector.append("option").text(id).property("value", id);  
         });
 
@@ -68,20 +65,16 @@ d3.json(urlData)
       gaugeChartFunc(meta_data[0]);
   });
 
-    //'value' is value of selected option in dropdown menu
-    function optionChanged(value) {
-    //Use .find() method to isolate 'id' in question in samples array  
-      const selectedId = samples.find((item) => item.id === value);
-    //(item) => function looks for desired element in meta_data array   
-    //by searching for case where 'id' is same as 'value' value passed to optionChanged function
+    
+    function optionChanged(value) {     //'value' = value of selected option in dropdown menu
+      const selectedId = samples.find((item) => item.id === value);    //Use .find() to isolate 'id' in question in samples array  
+                                                                      //(item) => function looks for desired element in meta_data array by searching for   
+                                                                      //case where 'id' is same as 'value' value passed to optionChanged function
       const demographicInfo = meta_data.find((item) => item.id == value);
 
     //Pass 'demographicInfo' and 'selectedId' as parameters to functions that will display data related to selected trial participant
-    //Demographic data
     metaDataFunc(demographicInfo);
-    //Bar chart  
     barChartFunc(selectedId);
-    //Bubble chart
     bubbleChartFunc(selectedId);
     gaugeChartFunc(demographicInfo);  
     }
@@ -117,14 +110,11 @@ d3.json(urlData)
           height: 500,
           width: 500,
         };
-      //Plot the BarChart
-        Plotly.newPlot("bar",chart, layout);
+        Plotly.newPlot("bar",chart, layout);     //Plot the BarChart
       }
 
-    //For trial participant in question, define and create bubble chart
-    //that displays 'otu_ids' for x values, 'sample_values' for y values,
-    //'sample_values' for  marker size, 'otu_ids' for marker colors, 
-    //'otu_labels' for text values
+    //For trial participant in question, define and create BUBBLE CHART that displays 'otu_ids' for x values, 
+    //'sample_values' for y values, 'sample_values' for  marker size, 'otu_ids' for marker colors, 'otu_labels' for text values
     function bubbleChartFunc(selectedId) {
       console.log("Bubble Chart Data:", selectedId);
       let x_axis = selectedId.otu_ids;
@@ -137,8 +127,7 @@ d3.json(urlData)
         x: x_axis,
         y: y_axis,
         text: text,
-        //Create scatter plot where marker represents each data point
-        mode: "markers", 
+        mode: "markers",       //Create scatter plot where marker represents each data point
         marker: {
           color: color,
           colorscale: "Rainbow",
@@ -148,13 +137,13 @@ d3.json(urlData)
       };
       let chart = [bubble];  
 
-      let layout = {
-        //use xaxis instead of x_axis
-        xaxis: {
+      let layout = {  
+        xaxis: {        //use xaxis instead of x_axis
           title: {text: "OTU ID"},
         },
       };
       Plotly.newPlot("bubble",chart, layout);
+    }
 
     function gaugeChartFunc(demographicInfo) {
       let wfreq = demographicInfo.wfreq;
@@ -190,4 +179,3 @@ d3.json(urlData)
 
   Plotly.newPlot("gauge", data, layout);
 }  
-    }
